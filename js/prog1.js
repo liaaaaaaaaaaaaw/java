4f4cb9faaafe38ef7c02927a16fa36c9fc385db8
liaaaaaaaaaaaaw <limartineztorrez@gmail.com>
2024-11-06 14:04:01 -0400

    Add files via upload

---
+++ js/prog1.js
@@ -0,0 +1,15 @@
+// calcular expresiones
+/*let t=prompt("Introducie valor de t");
+let r=(1/2)*Math.exp(2*t)-3.8*Math.pow(t,3);
+//t es elavado al cubo
+document.write("El resultado es: "+r.toFixed(2))
+function calcularExpresion(t) {
+    let r=(1/2)*Math.exp(2*t)-3.8*Math.pow(t,3);
+    return r.toFixed(2);
+}
+//solicitar el valor de t*/
+/*let t=prompt("Introducir valor de t");
+let resultado=calcularExpresion(parseFloat(t));
+//parsefloat convierte el dato a tipo float
+document.write("El resultado es: "+resultado);*/
+;
